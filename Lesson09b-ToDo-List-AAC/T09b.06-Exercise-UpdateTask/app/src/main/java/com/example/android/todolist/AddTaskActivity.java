b773ad36dbd56db86db8d9b799ec7c20a3a036af
KonstantinGlady <gladyi@hotmail.com>
2020-01-31 22:51:49 +0300

    update task

--- Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist;
 
@@ -73,15 +73,20 @@ public class AddTaskActivity extends AppCompatActivity {
                 // populate the UI
                 // TODO (3) Assign the value of EXTRA_TASK_ID in the intent to mTaskId
                 // Use DEFAULT_TASK_ID as the default
-
+                mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
                 // TODO (4) Get the diskIO Executor from the instance of AppExecutors and
                 // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(() -> {
+                    // TODO (5) Use the loadTaskById method to retrieve the task with id mTaskId and
+                    // assign its value to a final TaskEntry variable
+                    final TaskEntry task = mDb.taskDao().loadTaskById(mTaskId);
+                    // TODO (6) Call the populateUI method with the retrieve tasks
+                    // Rem ember to wrap it in a call to runOnUiThread
+                    runOnUiThread(() -> {
+                        populateUI(task);
+                    });
+                });
 
-                // TODO (5) Use the loadTaskById method to retrieve the task with id mTaskId and
-                // assign its value to a final TaskEntry variable
-
-                // TODO (6) Call the populateUI method with the retrieve tasks
-                // Remember to wrap it in a call to runOnUiThread
             }
         }
     }
@@ -115,8 +120,12 @@ public class AddTaskActivity extends AppCompatActivity {
      */
     private void populateUI(TaskEntry task) {
         // TODO (7) return if the task is null
-
+        if (task == null) {
+            return;
+        }
         // TODO (8) use the variable task to populate the UI
+        mEditText.setText(task.getDescription());
+        setPriorityInViews(task.getPriority());
     }
 
     /**
@@ -129,15 +138,17 @@ public class AddTaskActivity extends AppCompatActivity {
         Date date = new Date();
 
         final TaskEntry taskEntry = new TaskEntry(description, priority, date);
-        AppExecutors.getInstance().diskIO().execute(new Runnable() {
-            @Override
-            public void run() {
-                // TODO (9) insert the task only if mTaskId matches DEFAULT_TASK_ID
-                // Otherwise update it
-                // call finish in any case
+        AppExecutors.getInstance().diskIO().execute(() -> {
+            // TODO (9) insert the task only if mTaskId matches DEFAULT_TASK_ID
+            // Otherwise update it
+            // call finish in any case
+            if (mTaskId == DEFAULT_TASK_ID) {
                 mDb.taskDao().insertTask(taskEntry);
-                finish();
+            } else {
+                taskEntry.setId(mTaskId);
+                mDb.taskDao().updateTask(taskEntry);
             }
+            finish();
         });
     }
 

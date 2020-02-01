3f9c2d1fcb336990d7275703c381b9977eb06e7f
KonstantinGlady <gladyi@hotmail.com>
2020-02-01 14:41:33 +0300

    add LiveData to AddTaskActivity

--- Lesson09b-ToDo-List-AAC/T09b.08-Exercise-AddLiveDataToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.08-Exercise-AddLiveDataToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -1,25 +1,28 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -75,20 +78,21 @@ public class AddTaskActivity extends AppCompatActivity {
                 mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
 
 
-                        // TODO (3) Extract all this logic outside the Executor and remove the Executor
-                        // TODO (2) Fix compile issue by wrapping the return type with LiveData
-                         LiveData<TaskEntry> task = mDb.taskDao().loadTaskById(mTaskId);
-                        // TODO (4) Observe tasks and move the logic from runOnUiThread to onChanged
-                        // We will be able to simplify this once we learn more
-                        // about Android Architecture Components
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                // TODO (5) Remove the observer as we do not need it any more
-                                populateUI(task);
-                            }
-                        });
+                // TODO (3) Extract all this logic outside the Executor and remove the Executor
+                // TODO (2) Fix compile issue by wrapping the return type with LiveData
+                LiveData<TaskEntry> task = mDb.taskDao().loadTaskById(mTaskId);
+                // TODO (4) Observe tasks and move the logic from runOnUiThread to onChanged
+                // We will be able to simplify this once we learn more
+                // about Android Architecture Components
+                task.observe(this, new Observer<TaskEntry>() {
+                    @Override
+                    public void onChanged(@Nullable TaskEntry taskEntry) {
+                        Log.d(TAG, "Receiving database updtate from LiveData");
+                        task.removeObserver(this);
+                        AddTaskActivity.this.populateUI(taskEntry);
                     }
+                });
+
 
             }
         }
